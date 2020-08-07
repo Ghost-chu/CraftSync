e36eb134ca5d839cc53255ca26f8da7071998696
Ghost_chu <2908803755@qq.com>
2020-08-07 21:34:05 +0800

    PotionEffects for player

--- src/main/java/com/mcsunnyside/playersync/module/PlayerSyncModule.java
+++ src/main/java/com/mcsunnyside/playersync/module/PlayerSyncModule.java
@@ -4,10 +4,19 @@ import com.mcsunnyside.playersync.sync.Sync;
 import com.mcsunnyside.playersync.sync.SyncDataContainer;
 import com.mcsunnyside.playersync.sync.SyncModule;
 import com.mcsunnyside.playersync.sync.SyncType;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.logging.Logger;
+
 public class PlayerSyncModule implements SyncModule {
+    private final Logger logger = Logger.getLogger("PlayerSync");
     @Sync(field = "hp", type = SyncType.LOAD)
     public void loadPlayerHealth(@NotNull SyncDataContainer data) {
         double hp = Double.parseDouble(data.getData());
@@ -66,6 +75,35 @@ public class PlayerSyncModule implements SyncModule {
                 .player(player)
                 .data(String.valueOf(player.getMaximumAir()))
                 .build();
+    }
+
+    @Sync(field = "effects", type = SyncType.LOAD)
+    public void loadPlayerPotionEffects(@NotNull SyncDataContainer data) {
+        YamlConfiguration configuration = new YamlConfiguration();
+        try {
+            configuration.load(data.getData());
+        } catch (IOException | InvalidConfigurationException ioException) {
+            ioException.printStackTrace();
+            logger.warning("Failed to sync player " + data.getPlayer().getName() + "'s potion effects!");
+            return;
+        }
+        //noinspection unchecked
+        Collection<PotionEffect> effects = (Collection<PotionEffect>) configuration.getList("effects");
+        if (effects == null) {
+            return;
+        }
+        for (PotionEffectType value : PotionEffectType.values()) {
+            data.getPlayer().removePotionEffect(value);
+        }
+        data.getPlayer().addPotionEffects(effects);
+    }
+
+    @NotNull
+    @Sync(field = "effects", type = SyncType.SAVE)
+    public SyncDataContainer savePlayerPotionEffects(@NotNull Player player) {
+        YamlConfiguration configuration = new YamlConfiguration();
+        configuration.set("effects", player.getActivePotionEffects());
+        return SyncDataContainer.builder().player(player).data(configuration.saveToString()).build();
     }
 
 }
