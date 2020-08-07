ca3f59bebb20467dc901731998efba81a729b3ae
Ghost_chu <2908803755@qq.com>
2020-08-07 21:22:55 +0800

    Javadoc

--- src/main/java/com/mcsunnyside/playersync/sync/Sync.java
+++ src/main/java/com/mcsunnyside/playersync/sync/Sync.java
@@ -4,9 +4,24 @@ package com.mcsunnyside.playersync.sync;
 import org.jetbrains.annotations.NotNull;
 
 public @interface Sync {
+   /**
+    * Unique ID for Sync Item, used as column name
+    *
+    * @return The Unique ID
+    */
    @NotNull String field() default "unknown";
 
+   /**
+    * Controls the sync start at
+    *
+    * @return Default is JOIN, QUIT and TIMER
+    */
    @NotNull SyncTime[] sync() default {SyncTime.JOIN, SyncTime.QUIT, SyncTime.TIMER};
 
+   /**
+    * Mark as this method use for load or save between database
+    *
+    * @return The SyncType
+    */
    @NotNull SyncType type() default SyncType.LOAD;
 }
