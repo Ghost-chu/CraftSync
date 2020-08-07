a7b95c0fc9aad1b1035dac738fdba6d53e91b7ff
Ghost_chu <2908803755@qq.com>
2020-08-08 00:05:35 +0800

    delombok

--- src/main/java/com/mcsunnyside/playersync/sync/SyncDataContainer.java
+++ src/main/java/com/mcsunnyside/playersync/sync/SyncDataContainer.java
@@ -1,16 +1,90 @@
 package com.mcsunnyside.playersync.sync;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.bukkit.entity.Player;
 
-@Data
-@AllArgsConstructor
-@Builder
-@EqualsAndHashCode
 public class SyncDataContainer {
     private Player player;
     private String data;
+
+    public SyncDataContainer(Player player, String data) {
+        this.player = player;
+        this.data = data;
+    }
+
+    public static SyncDataContainerBuilder builder() {
+        return new SyncDataContainerBuilder();
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String toString() {
+        return "SyncDataContainer(player=" + this.getPlayer() + ", data=" + this.getData() + ")";
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof SyncDataContainer)) return false;
+        final SyncDataContainer other = (SyncDataContainer) o;
+        if (!other.canEqual(this)) return false;
+        final Object this$player = this.getPlayer();
+        final Object other$player = other.getPlayer();
+        if (this$player == null ? other$player != null : !this$player.equals(other$player)) return false;
+        final Object this$data = this.getData();
+        final Object other$data = other.getData();
+        return this$data == null ? other$data == null : this$data.equals(other$data);
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof SyncDataContainer;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $player = this.getPlayer();
+        result = result * PRIME + ($player == null ? 43 : $player.hashCode());
+        final Object $data = this.getData();
+        result = result * PRIME + ($data == null ? 43 : $data.hashCode());
+        return result;
+    }
+
+    public static class SyncDataContainerBuilder {
+        private Player player;
+        private String data;
+
+        SyncDataContainerBuilder() {
+        }
+
+        public SyncDataContainer.SyncDataContainerBuilder player(Player player) {
+            this.player = player;
+            return this;
+        }
+
+        public SyncDataContainer.SyncDataContainerBuilder data(String data) {
+            this.data = data;
+            return this;
+        }
+
+        public SyncDataContainer build() {
+            return new SyncDataContainer(player, data);
+        }
+
+        public String toString() {
+            return "SyncDataContainer.SyncDataContainerBuilder(player=" + this.player + ", data=" + this.data + ")";
+        }
+    }
 }
--- src/main/java/com/mcsunnyside/playersync/sync/SyncModuleRegister.java
+++ src/main/java/com/mcsunnyside/playersync/sync/SyncModuleRegister.java
@@ -1,18 +1,112 @@
 package com.mcsunnyside.playersync.sync;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 import java.lang.reflect.Method;
 
-@AllArgsConstructor
-@Builder
-@Data
-@EqualsAndHashCode
 public class SyncModuleRegister {
     private Method method;
     private SyncModule moduleObject;
     private Sync sync;
+
+    public SyncModuleRegister(Method method, SyncModule moduleObject, Sync sync) {
+        this.method = method;
+        this.moduleObject = moduleObject;
+        this.sync = sync;
+    }
+
+    public static SyncModuleRegisterBuilder builder() {
+        return new SyncModuleRegisterBuilder();
+    }
+
+    public Method getMethod() {
+        return this.method;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
+    }
+
+    public SyncModule getModuleObject() {
+        return this.moduleObject;
+    }
+
+    public void setModuleObject(SyncModule moduleObject) {
+        this.moduleObject = moduleObject;
+    }
+
+    public Sync getSync() {
+        return this.sync;
+    }
+
+    public void setSync(Sync sync) {
+        this.sync = sync;
+    }
+
+    public String toString() {
+        return "SyncModuleRegister(method=" + this.getMethod() + ", moduleObject=" + this.getModuleObject() + ", sync=" + this.getSync() + ")";
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof SyncModuleRegister)) return false;
+        final SyncModuleRegister other = (SyncModuleRegister) o;
+        if (!other.canEqual(this)) return false;
+        final Object this$method = this.getMethod();
+        final Object other$method = other.getMethod();
+        if (this$method == null ? other$method != null : !this$method.equals(other$method)) return false;
+        final Object this$moduleObject = this.getModuleObject();
+        final Object other$moduleObject = other.getModuleObject();
+        if (this$moduleObject == null ? other$moduleObject != null : !this$moduleObject.equals(other$moduleObject))
+            return false;
+        final Object this$sync = this.getSync();
+        final Object other$sync = other.getSync();
+        return this$sync == null ? other$sync == null : this$sync.equals(other$sync);
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof SyncModuleRegister;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $method = this.getMethod();
+        result = result * PRIME + ($method == null ? 43 : $method.hashCode());
+        final Object $moduleObject = this.getModuleObject();
+        result = result * PRIME + ($moduleObject == null ? 43 : $moduleObject.hashCode());
+        final Object $sync = this.getSync();
+        result = result * PRIME + ($sync == null ? 43 : $sync.hashCode());
+        return result;
+    }
+
+    public static class SyncModuleRegisterBuilder {
+        private Method method;
+        private SyncModule moduleObject;
+        private Sync sync;
+
+        SyncModuleRegisterBuilder() {
+        }
+
+        public SyncModuleRegister.SyncModuleRegisterBuilder method(Method method) {
+            this.method = method;
+            return this;
+        }
+
+        public SyncModuleRegister.SyncModuleRegisterBuilder moduleObject(SyncModule moduleObject) {
+            this.moduleObject = moduleObject;
+            return this;
+        }
+
+        public SyncModuleRegister.SyncModuleRegisterBuilder sync(Sync sync) {
+            this.sync = sync;
+            return this;
+        }
+
+        public SyncModuleRegister build() {
+            return new SyncModuleRegister(method, moduleObject, sync);
+        }
+
+        public String toString() {
+            return "SyncModuleRegister.SyncModuleRegisterBuilder(method=" + this.method + ", moduleObject=" + this.moduleObject + ", sync=" + this.sync + ")";
+        }
+    }
 }
